bb6865882ffee363c2f4c1167a6701f846a13478
Dasaradh <dasarathram143@gmail.com>
2023-05-17 22:22:11 +0530

    Hello flutter added icons in nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,102 @@ void main() {
   runApp(const MyWidget());
 }
 
-
 class MyWidget extends StatelessWidget {
   const MyWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(
-        child: Text("hello flutter"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello flutter'),
+          backgroundColor: Colors.deepOrange.shade500,
+          leading: const Icon(Icons.home_rounded),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("search"),
+                IconButton(
+                  icon: const Icon(
+                    Icons.search,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.flutter_dash_rounded,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.facebook,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.snapchat,
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.tealAccent,
+                alignment: Alignment.center,
+                width: 250,
+                height: 500,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                child: const Image(
+                    image: NetworkImage(
+                        'https://images.pexels.com/photos/16678541/pexels-photo-16678541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')),
+              ),
+              Container(
+                color: Colors.teal,
+                alignment: Alignment.center,
+                width: 250,
+                height: 500,
+                padding: const EdgeInsets.all(30),
+                child: const Image(
+                  image: NetworkImage(
+                      'https://images.pexels.com/photos/16678541/pexels-photo-16678541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                ),
+              ),
+              Container(
+                color: Colors.tealAccent,
+                alignment: Alignment.center,
+                width: 250,
+                height: 500,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                child: const Image(
+                  image: NetworkImage(
+                      'https://images.pexels.com/photos/16678541/pexels-photo-16678541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
-
 }
+
+// IconButton(
+// icon: const Icon(
+// Icons.facebook,
+// color: Colors.blue,
+// ),
+// onPressed: () {},
+// )
